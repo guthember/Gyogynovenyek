2c846ddee5bd681176c0a5807182f07c114c5bf5
guthember <guthember@gmail.com>
2020-10-12 12:54:30 +0200

    Add project files.

---
+++ Gyogynovenyek/Megoldas.md
@@ -0,0 +1,67 @@
+﻿
+# Megoldás
+
+## Osztály létrehozása
+- Kétféle mód közül bármelyik
+  - Ctrl + .
+  - Adjunk hozzá egy osztályt
+- 5 tulajdonság 
+  - public get
+  - private set
+- 1 konstruktor ?
+  - 1 paraméter --> string
+  - például: "Acsalapu;levél;6;8"
+  - ;-vel vannak elválasztva
+  - 4 darab adat --> 5 tulajdonság?
+  - átadott, fáljból beolvasott sort:
+    - ';' mentén tagoljuk
+    - [0] --> Nev
+    - [1] --> Resz
+    - [2] --> Kezd
+    - [3] --> Veg
+    - Idotartam --> számolás (lásd következő pont)
+- 5-dik tulajdonság --> Időtartam
+    - Konstruktorban számoljuk
+    - számolása: Vég - Kezdet HA!! Vég > Kezdet
+    - Vég > Kezdet --> Vég - Kezdet + 1
+    - Egyébként --> 12 - Kezdet + Vég + 1
+
+## Adatok beolvasása és eltárolása
+- Listában tároljuk az objektum példányokat
+- Olvassuk be:
+  - System.IO
+  - StreamReader
+  - while ciklussal --> amíg tart a fájl
+  - Cikluson belül listához hozzáadunk egy új objektum példányt (beolvasott sor a paraméter)
+
+## 1. Feladat
+- Hány növény van (lista Count tuljadonság)
+
+## 2. Feladat
+- Milyen gyűjthető részek vannak?
+- Hány darab van ezekből
+- Adatszerk: Dictionary<string, int>
+- 1 Milyen gyűjthető részek
+  - Foreach "novenyek"
+  - Ha a rész nem kulcs --> akkor adjuk hozzá 0-val
+- 2 Hány darab van ezekből
+  - Foreach "novenyek"
+  - Ha "Resz" == Key --> Value++
+
+## 3. Feladat
+- Maximum keresés --> Időtartam alapján
+  - maximum változó = 0
+  - foreach novenyek 
+  - ha max < Idotartam --> max = Idotartam
+- Növény(ek) amely(ek) a legtöbb ideig szedtek
+    - foreach novenyek
+    - Kiírni ha Idotartam == max
+
+## 4. Feladat
+- Időtartam átlag számítás
+  - szum változó = 0 (double)
+  - atlag változó
+  - foreach novenyek 
+  - szum += Idotartam
+  - atlag = szum / Count
+
---
+++ Gyogynovenyek/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyogynovenyek
+{
+  class Program
+  {
+    static List<Noveny> novenyek = new List<Noveny>();
+    static Dictionary<string, int> reszek = new Dictionary<string, int>();
+
+    static void Beolvas()
+    {
+      StreamReader file = new StreamReader("noveny.csv");
+
+      while (!file.EndOfStream)
+      {
+        novenyek.Add(new Noveny(file.ReadLine()));
+      }
+
+      file.Close();
+    }
+
+    static void ElsoFeladat()
+    {
+      Console.WriteLine("1. feladat: Növények száma: {0}",
+        novenyek.Count);
+    }
+
+    static void MasodikFeladat()
+    {
+      Console.WriteLine("\n2. feladat: Gyűjthető részek:");
+      foreach (var n in novenyek)
+      {
+        if (!reszek.ContainsKey(n.Resz))
+        {
+          reszek.Add(n.Resz, 0);
+        }
+      }
+
+      foreach (var n in novenyek)
+      {
+        reszek[n.Resz]++;
+      }
+
+      foreach (var r in reszek)
+      {
+        Console.WriteLine($"{r.Key}: {r.Value}");
+      }
+
+    }
+
+    static void HarmadikFeladat()
+    {
+      Console.Write("\n3. feladat: Legtöbb idő amíg gyűjthető: ");
+      int max = 0;
+
+      foreach (var n in novenyek)
+      {
+        if (max < n.Idotartam)
+        {
+          max = n.Idotartam;
+        }
+      }
+
+      Console.WriteLine(max);
+      Console.WriteLine("Növény(ek):");
+      foreach (var n in novenyek)
+      {
+        if (n.Idotartam == max)
+        {
+          Console.WriteLine(n.Nev);
+        }
+      }
+
+
+    }
+
+    static void NegyedikFeladat()
+    {
+      double szum = 0;
+      foreach (var n in novenyek)
+      {
+        szum += n.Idotartam;
+      }
+
+      Console.WriteLine("\n4. feladat: Átlagos gyűjthető időtartam: {0:N2}",
+        szum / novenyek.Count
+        );
+    }
+
+
+    static void Main(string[] args)
+    {
+
+      Beolvas();
+      ElsoFeladat();
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+
+      Console.ReadLine();
+    }
+  }
+}
